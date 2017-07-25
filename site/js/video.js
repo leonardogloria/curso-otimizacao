cd7054e455076b1a5045138538ce7b4301563bdb
= <=>
2017-07-25 13:13:11 -0300

    Finalizando Imagens

---
+++ site/js/video.js
@@ -0,0 +1,5 @@
+setTimeout(function(){
+	var iframe = '<iframe src="https://player.vimeo.com/video/85147502" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+	document.querySelector('.iframe-video').innerHTML = iframe
+
+},1500)
